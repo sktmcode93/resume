ee97f17080acd6864a2081f81d811ca1d835e2d9
sktmcode93 <sktmcode93@gmail.com>
2022-06-04 16:26:01 +0900

    add : addDday Component

---
+++ src/Components/dday/AddDday.jsx
@@ -0,0 +1,42 @@
+import { changeObjVal } from "js/common";
+import { memo, useEffect } from "react";
+
+const AddDday = ({ addState, setAddState, setAddModal, setDayList }) => {
+    const outFn = ({ path }) => {
+        for (let { className } of path) {
+            if (className && className === "add-d-day") return;
+        }
+        window.removeEventListener("click", outFn);
+        setAddModal(false);
+    }
+    useEffect(() => { window.addEventListener("click", outFn); }, []);
+
+    const complete = () => {
+        console.log(addState.dday)
+        return;
+        setDayList(prev => {
+            const clone = [...prev];
+            clone.push(addState);
+            return clone;
+        })
+
+        window.removeEventListener("click", outFn);
+        setAddModal(false);
+        setAddState({ title: "", dday: "" })
+    }
+    return <section className="add-d-day">
+        <label>
+            <h5>디데이명</h5>
+            <input type="text" value={addState.title} onChange={e => changeObjVal("title", e.target.value, setAddState)} />
+        </label>
+        <label>
+            <h5>날짜</h5>
+            <input type="date" value={addState.dday} onChange={e => changeObjVal("dday", e.target.value, setAddState)} />
+        </label>
+        <div>
+            <button onClick={complete}>추가하기</button>
+        </div>
+    </section>
+}
+
+export default memo(AddDday);
