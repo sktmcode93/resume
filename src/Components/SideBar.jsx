5708285e5d50769ca1181a241bfbc6ef5d465d17
sktmcode93 <sktmcode93@gmail.com>
2022-06-04 16:25:10 +0900

    modify : active Tab on SideBar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,9 +1,11 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 
 import myImg from 'images/김성엽.jpg';
 
 const SideBar = () => {
+    const { pathname } = useLocation();
     return <aside className="side-bar">
         <div className="img-box">
             <img src={myImg} alt="증명사진" />
@@ -13,9 +15,9 @@ const SideBar = () => {
             <h2>프론트엔드 개발자</h2>
         </div>
         <ul className="nav-list">
-            <li><Link to="/">소개</Link></li>
-            <li><Link to="/skills">기술</Link></li>
-            <li><Link to="/portfolio">포트폴리오</Link></li>
+            <li className={pathname === "/" ? "active" : ""}><Link to="/">소개</Link></li>
+            <li className={pathname === "/skills" ? "active" : ""}><Link to="/skills">기술</Link></li>
+            <li className={pathname === "/portfolio" ? "active" : ""}><Link to="/portfolio">포트폴리오</Link></li>
         </ul>
     </aside>
 }
