6c4eec15865ec1ec67ce5391086b3a0da3bb4633
sktmcode93 <sktmcode93@gmail.com>
2022-06-04 15:46:40 +0900

    little change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router";
 import Home from "Components/Home";
 import Skills from "Components/Skills";
 import PortFolio from "Components/Portfolio";
+import Dday from "Components/dday/Dday";
+import NotFound from 'Components/NotFound';
 
 import 'styles/css/main.css';
 
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/portfolio" element={<PortFolio />} />
+        <Route path="/d-day-counter" element={<Dday />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
