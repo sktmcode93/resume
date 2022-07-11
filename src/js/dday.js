7ab5d9520fbd510bfef63f0b1cc3b2a633ab2ef2
sktmcode93 <sktmcode93@gmail.com>
2022-07-11 15:52:32 +0900

    modify : 시간 구하는 기능 분리 && add : 디데이 리스트 초기화 기능

---
+++ src/js/dday.js
@@ -0,0 +1,27 @@
+// dday에 따라 남은 시간을 구해주는 함수
+export const findLeftTimes = (dday, now) => {
+    const wholeTime = parseInt(((new Date(dday)) - now) / 1000);
+    const leftSecs = wholeTime % 60;
+    const leftMins = parseInt(wholeTime / 60) % 60;
+    const leftHours = parseInt(wholeTime / 3600) % 24;
+    const leftDays = parseInt(wholeTime / (3600 * 24));
+    if (wholeTime < 0) return { "일이 지났어요": Math.abs(leftDays) };
+    else {
+        const lefts = {
+            d: leftDays,
+            h: leftHours,
+            m: leftMins,
+            s: leftSecs
+        }
+        for (const k in lefts) {
+            if (!lefts[k]) delete lefts[k];
+        }
+        return lefts;
+    }
+}
+// 디데이 리스트를 초기화 하는 함수
+export const clearDDay = setDayList => {
+    if (!window.confirm("디데이 리스트를 초기화 시키시겠습니까?")) return;
+    localStorage.setItem("dday", "");
+    setDayList([]);
+}
