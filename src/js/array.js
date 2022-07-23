0bc83e0c0e9c9f5813ccfcc3edb547b60cf17636
sktmcode93 <sktmcode93@gmail.com>
2022-07-23 23:15:18 +0900

    add : 투두리스트 에러 리스트 추가

--- src/js/array.js
+++ src/js/array.js
@@ -3,5 +3,6 @@ export const docsList = ["html", "jsx", "js", "css", "scss"];
 export const errorList = {
     todo_fill_category: "카테고리를 채워주세요.",
     todo_reserved_category: "해당 이름으로는 생성할 수 없습니다.",
-
+    todo_duple: "이미 존재하는 값입니다.",
+    todo_delete_standard: "기본 항목은 제거 할 수 없습니다."
 }
