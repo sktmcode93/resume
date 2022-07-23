795abffc476ba100f2a5374ca5cc6efcccd9852f
sktmcode93 <sktmcode93@gmail.com>
2022-07-23 13:41:20 +0900

    add : 에러리스트 추가

--- src/js/array.js
+++ src/js/array.js
@@ -1,1 +1,7 @@
 export const docsList = ["html", "jsx", "js", "css", "scss"];
+
+export const errorList = {
+    todo_fill_category: "카테고리를 채워주세요.",
+    todo_reserved_category: "해당 이름으로는 생성할 수 없습니다.",
+
+}
