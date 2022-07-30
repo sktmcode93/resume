0c315627d2eccdc0248f51626704a7a9f16586d9
sktmcode93 <sktmcode93@gmail.com>
2022-07-30 14:25:07 +0900

    add : 리스트와 디테일 정보를 가지고 오는 API 추가

--- src/js/serviceApi.js
+++ src/js/serviceApi.js
@@ -1,5 +1,24 @@
 import axios from "axios";
 
+export const api__getMovieList = async (sorting, page, limit) => {
+    const subCondition = `sort_by=${sorting}&page=${page}&limit=${limit}`;
+    try {
+        return await axios.get(`https://yts.mx/api/v2/list_movies.json?${subCondition}`);
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export const api__getMovieDetail = async id => {
+    try {
+        return await axios.get(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
 export const api__getMovieByQueryTerm = async query_term => {
     try {
         return axios.get(`https://yts.mx/api/v2/list_movies.json?query_term=${query_term}`);
